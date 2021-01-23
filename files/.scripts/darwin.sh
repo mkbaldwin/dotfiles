9b946099787b0e613715f3ba001106661caf9518
Michael Baldwin <mkbaldwin@users.noreply.github.com>
2021-01-22 20:25:29 -0500

    Adding in the code command for VS code when running on darwin

--- files/.scripts/darwin.sh
+++ files/.scripts/darwin.sh
@@ -9,3 +9,15 @@ export BASH_SILENCE_DEPRECATION_WARNING=1
 # Custom Command Aliases that are macOS specific
 ###############################################################################
 alias ll='ls -lahG'
+
+
+###############################################################################
+# macOS Path Additions
+###############################################################################
+
+# Add VSCode to the path (if available)
+if [ -d "/Applications/Visual Studio Code.app/" ]; then
+  export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+elif [ -d "/Applications/Visual Studio Code - Insiders.app/" ]; then
+  export PATH="$PATH:/Applications/Visual Studio Code - Insiders.app/Contents/Resources/app/bin"
+fi 
