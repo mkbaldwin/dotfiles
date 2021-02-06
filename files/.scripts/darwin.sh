1eb42954c938907bc63fe146f99665cfbe61df0e
Michael Baldwin <mkbaldwin@users.noreply.github.com>
2021-02-05 19:51:31 -0500

    Adding in scripts for homebrew on macOS

--- files/.scripts/darwin.sh
+++ files/.scripts/darwin.sh
@@ -21,3 +21,18 @@ if [ -d "/Applications/Visual Studio Code.app/" ]; then
 elif [ -d "/Applications/Visual Studio Code - Insiders.app/" ]; then
   export PATH="$PATH:/Applications/Visual Studio Code - Insiders.app/Contents/Resources/app/bin"
 fi 
+
+# Determine if homebrew is installed and add to the path
+HOMEBREW_PREFIX="/opt/homebrew"
+if [ -d "$HOMEBREW_PREFIX" ]; then
+  export HOMEBREW_PREFIX
+  export HOMEBREW_CELLAR="/opt/homebrew/Cellar"
+  export HOMEBREW_REPOSITORY="/opt/homebrew"
+  export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}"
+  export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:"
+  export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}"
+fi
+
+
+# Tell Homebrew not to share analytics
+export HOMEBREW_NO_ANALYTICS=1
