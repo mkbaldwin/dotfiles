c86bc46990b19a93200a0b9844f18341332077a4
Michael Baldwin <mkbaldwin@users.noreply.github.com>
2021-04-17 18:18:12 -0400

    Adding in RVM

--- files/.scripts/extras/rvm.sh
+++ files/.scripts/extras/rvm.sh
@@ -8,6 +8,7 @@ RVM_DIR="$HOME/.rvm"
 if [ -d "$RVM_DIR" ]; then
   # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
   export PATH="$PATH:$RVM_DIR/bin"
+  source ~/.rvm/scripts/rvm
 else
   unset RVM_DIR
 fi
