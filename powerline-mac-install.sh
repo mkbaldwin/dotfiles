3e4a1a358c531f12adc3364d09014227846f5228
Michael Baldwin <mkbaldwin@users.noreply.github.com>
2021-06-10 08:46:24 -0400

    Adding more configuration files for local dotfiles

---
+++ powerline-mac-install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+
+brew install svn
+brew install homebrew/cask-fonts/font-droid-sans-mono-for-powerline
+brew install homebrew/cask-fonts/font-fira-mono-for-powerline
+brew install homebrew/cask-fonts/font-powerline-symbols
+brew install homebrew/cask-fonts/font-roboto-mono-for-powerline
+brew install powerline-go
