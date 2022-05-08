5790facd0868d0e51f53aea70ec7ee377e8fdb2d
Michael Baldwin <mkbaldwin@users.noreply.github.com>
2022-05-08 16:02:42 -0400

    Updating the powerline configuration to support powerline-go since the other powerline utility depended on python2 that is no longer available by default on macos.

--- files/.scripts/extras/powerline.sh
+++ files/.scripts/extras/powerline.sh
@@ -1,17 +1,26 @@
 #! /bin/bash
 
 ###############################################################################
-# Install powerline as our prompt if it is installed and available
+# Install powerline-go as our prompt if it is installed and available
 ###############################################################################
 
 # Configure powerline shell as our prompt if it exists.
 # TODO: Make this work on macOS
-if [[ -f /usr/local/bin/powerline-shell ]]; then
+if [[ -f /opt/homebrew/bin/powerline-go ]]; then
     function _update_ps1() {
-                PS1=$(powerline-shell $?)
-        }
+        OPTS="-colorize-hostname -numeric-exit-codes -newline"
 
-    if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then
-                PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+        PS1="$(/opt/homebrew/bin/powerline-go -error $? $OPTS -jobs $(jobs -p | wc -l))"
+
+        # Uncomment the following line to automatically clear errors after showing
+        # them once. This not only clears the error for powerline-go, but also for
+        # everything else you run in that shell. Don't enable this if you're not
+        # sure this is what you want.
+
+        #set "?"
+    }
+
+    if [ "$TERM" != "linux" ] && [ -f "/opt/homebrew/bin/powerline-go" ]; then
+        PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
     fi
 fi
