915ea54ef27fcec4c1b8639ca3859d409c2df33d
Michael Baldwin <mkbaldwin@users.noreply.github.com>
2023-04-09 11:15:15 -0400

    Fix vasm scripts for working on my mac, and add cc65 as well.

---
+++ files/.scripts/extras/cc65.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+###############################################################################
+# Look to see if cc65 is located in an expected location and add to path
+#
+# cc65 is a C compiler for 6502 microprocessors. 
+#    https://cc65.github.io/
+###############################################################################
+CC65_DIR=/opt/cc65
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    CC65_DIR=~/dev-tools/cc65/bin
+fi
+
+if [ -d "$CC65_DIR" ]; then
+    export PATH="$CC65_DIR:$PATH"
+else
+    unset CC65_DIR
+fi
--- files/.scripts/extras/vasm.sh
+++ files/.scripts/extras/vasm.sh
@@ -7,9 +7,13 @@
 #    http://sun.hasenbraten.de/vasm
 ###############################################################################
 VASM_DIR=/opt/vasm
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    VASM_DIR=~/dev-tools/vasm
+fi
 
 if [ -d "$VASM_DIR" ]; then
     export PATH="$VASM_DIR:$PATH"
+    alias vasm="$VASM_DIR/vasm6502_oldstyle"
 else
     unset VASM_DIR
 fi
